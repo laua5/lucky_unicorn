e8f193aee04548cb2a71f43f50ec60e2bc533545
laua5 <laua5@middleton.school.nz>
2023-04-05 12:20:55 +1200

    Update 00_LU_base_v1.py

--- 00_LU_base_v1.py
+++ 00_LU_base_v1.py
@@ -30,8 +30,27 @@ def instructions():
     print()
     print("The rules of the game will go here")
     print()
-    print("Program continues")
-    print()
+
+
+# number checking function
+def num_check(question, low, high):
+    error = "That was not a valid input\n" \
+            "Please enter a number between {} and {}\n".format(low, high)
+
+    # Keep asking until a valid amount (1-10) is entered
+    while True:
+        try:
+            # ask for amount
+            response = int(input(question))
+
+            # check for number within the required range
+            if low <= response <= high:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
 
 
 # Main routine go here.....
@@ -39,5 +58,7 @@ played_before = yes_no("Have you played this game before? ")
 
 if played_before == "No":
     instructions()
-else:
-    print("Program continues")
+
+# Ask how much they want to play with
+user_balance = num_check("How much would you like to play with? $", 1, 10)
+print(f"You are playing with ${user_balance}")
