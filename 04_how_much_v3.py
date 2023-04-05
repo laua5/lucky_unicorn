c3cd87e25187d92b350924c5df77c4d6a3d73f47
laua5 <laua5@middleton.school.nz>
2023-04-05 12:21:04 +1200

    Create 04_how_much_v3.py

---
+++ 04_how_much_v3.py
@@ -0,0 +1,20 @@
+""" Component 2 (how much) v3
+More efficient method - includes valid range as the basis of the while loop
+which eliminates the need to use the 'valid' variable """
+
+error = "That was not a valid input\n"
+user_balance = 0
+
+# Keep asking until a valid amount (1-10) is entered
+while not 1 <= user_balance <= 10:
+    try:
+        # ask for amount
+        user_balance = int(input("Please enter a whole number between 1 and 10"
+                                 "\nHow much would you like to play with? $"))
+
+        print()
+
+    except ValueError:
+        print(error)
+
+print(f"You are play with ${user_balance}")
