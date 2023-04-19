6efd3cf4ea2ea4e534f2e9e467b616c90dfa0a50
laua5 <laua5@middleton.school.nz>
2023-04-19 17:15:51 +1200

    Create 07_formatter_v1.py

---
+++ 07_formatter_v1.py
@@ -0,0 +1,13 @@
+""" Component 5 - statement formatter v1
+"""
+
+symbol = "*"
+text = "hello world"
+sides = "$" * 3
+
+formatted_text = f"{sides} {text} {sides}"
+top_bottom = "*" * len(formatted_text)
+
+print(top_bottom)
+print(formatted_text)
+print(top_bottom)
