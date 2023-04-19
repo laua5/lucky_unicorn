fb519a5a65218923a30d8f58f9ecb2809aebcae0
laua5 <laua5@middleton.school.nz>
2023-04-19 17:15:46 +1200

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,13 @@ import random
 for item in range(20):
     number = random.randint(1, 100)
     print(number, end="\t")
+
+text = "hello world"
+sides = "$" * 3
+
+formatted_text = f"{sides} {text} {sides}"
+top_bottom = "*" * len(formatted_text)
+
+print(top_bottom)
+print(formatted_text)
+print(top_bottom)
